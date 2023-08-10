fce3d6079f7e5d16be50d564b747905065aed997
James Hales <jdhales2686@gmail.com>
2023-08-10 17:14:46 -0600

    added auth token model

--- models/auth_tokens.py
+++ models/auth_tokens.py
@@ -38,32 +38,30 @@ import marshmallow as ma
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
 from db import db
-from datetime import datetime, timedelta
 from .users import UsersSchema
 
 
-# class Auths(db.Model):
-#     __tablename__ = "AuthTokens"
+class Auths(db.Model):
+    __tablename__ = "AuthTokens"
 
-#     auth_token = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-#     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey("Users.user_id"), nullable=False)
-#     expiration = db.Column(db.DateTime(), nullable=False, default=datetime.now() + timedelta(12))
+    github_token = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey("Users.user_id"), nullable=False)
 
-#     def __init__(self, user_id, expiration):
-#         self.user_id = user_id
-#         self.expiration = expiration
+    def __init__(self, github_token, user_id):
+        self.github_token = github_token
+        self.user_id = user_id
 
-#     def new_auth():
-#         return Auths(0, "")
-
-
-# class AuthsSchema(ma.Schema):
-#     class Meta:
-#         fields = ['auth_token', 'user_id', 'expiration']
-
-#         user_id = ma.fields.Nested(UsersSchema)
-#         # user = ma.fields.Nested(UsersSchema(only=("role", "first_name", "user_id")))
+    def new_auth():
+        return Auths("", "")
 
 
-# auth_schema = AuthsSchema()
-# auths_schema = AuthsSchema(many=True)
+class AuthsSchema(ma.Schema):
+    class Meta:
+        fields = ['auth_token', 'user_id']
+
+        user_id = ma.fields.Nested(UsersSchema)
+        # user = ma.fields.Nested(UsersSchema(only=("role", "first_name", "user_id")))
+
+
+auth_schema = AuthsSchema()
+auths_schema = AuthsSchema(many=True)

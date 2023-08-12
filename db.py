f4774cec26dba0990bcecfc53958832499beb3e2
James Hales <jdhales2686@gmail.com>
2023-08-12 16:20:40 -0600

    functionality for db and init

--- db.py
+++ db.py
@@ -1,15 +1,15 @@
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.dialects.postgresql import UUID
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUI
 
-# __all__ = ('db', 'init_db')
+__all__ = ('db', 'init_db')
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
 
-# def init_db(app=None, db=None):
-#     if isinstance(app, Flask) and isinstance(db, SQLAlchemy):
-#         db.init_app(app)
+def init_db(app=None, db=None):
+    if isinstance(app, Flask) and isinstance(db, SQLAlchemy):
+        db.init_app(app)
 
-#     else:
-#         raise ValueError("Cannot init DB wihtout db and app objects")
+    else:
+        raise ValueError("Unable to init database without database and app objects")

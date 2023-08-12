8ef26fc106b692c427065f306cb3f73b2e9ddb9f
James Hales <jdhales2686@gmail.com>
2023-08-12 15:58:03 -0600

    implemented JSONType from sqlalchemy_utils

--- models/sessions.py
+++ models/sessions.py
@@ -2,6 +2,7 @@ import marshmallow as ma
 import uuid
 from datetime import datetime, time
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy_utils import JSONType
 
 from models.repositories import RepositoriesSchema
 from db import db
@@ -15,7 +16,7 @@ class Sessions(db.model):
     num_of_commits = db.Column(db.integer(), default=1, nullable=False)
     commit_by_repo_ammount = db.Column(db.Boolean(), default=True, nullable=False)  # committing by a num of repos or a num of commits
     time_to_commit = db.Column(db.DateTime.time())  # default=datetime.combine(datetime.min, time(8, 17)
-    time_frame = db.Column(db.dictionary(), default={"8:17": "8:17"}, nullable=False)
+    time_frame = db.Column(db.JSONType(), default={"8:17": "8:17"}, nullable=False)
     latest_commit = db.Column(db.DateTime())
     current_position = db.Column(db.integer(), default=0, nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)

811a064965c5735dfd0387b66232e4455f7abeac
James Hales <jdhales2686@gmail.com>
2023-08-12 16:17:36 -0600

    repositories model built out

---
+++ models/repositories.py
@@ -0,0 +1,35 @@
+import marshmallow as ma
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy_utils import ScalarListType as ListType
+
+from db import db
+from models.users import UsersSchema
+
+
+class Repositories(db.model):
+    __tablename__ = "Muclses"
+
+    repo_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = db.Column(UUID(as_uuid=True), db.ForeignKey("Users.user_id"), nullable=False)
+    name = db.Column(db.String(), nullable=False)
+    branches = db.Column(db.ListType())
+
+    def __init__(self, user_id, name, branches):
+        self.user_id = user_id
+        self.name = name
+        self.branches = branches
+
+    def new_repository():
+        return Repositories("", "", [])
+
+
+class RepoSchema(ma.Schema):
+    class Meta:
+        fields = ['repo_id', 'user_id', 'name', 'branches']
+
+        user_id = ma.fields.Nested(UsersSchema)
+
+
+repo_schema = RepoSchema()
+repos_schema = RepoSchema(many=True)

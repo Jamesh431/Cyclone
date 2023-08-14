a6ed85061c779f7f67cbda79bab1cadd9643ba69
James Hales <jdhales2686@gmail.com>
2023-08-14 16:23:56 -0600

    initializes and creates database tables. Then starts server

--- app.py
+++ app.py
@@ -2,3 +2,44 @@ from flask import Flask, request, jsonify
 from db import *
 import os
 from flask_marshmallow import Marshmallow
+
+from models.auth_tokens import Auths
+from models.users import Users
+from models.sessions import Sessions
+from models.repositories import Repositories
+from models.commits import Commits
+from models.user_sessions_xref import user_sessions_xref
+import routes
+
+database_pre = os.environ.get("DATABASE_PRE")
+database_addr = os.environ.get("DATABASE_ADDR")
+database_user = os.environ.get("DATABASE_USER")
+database_port = os.environ.get("DATABASE_PORT")
+database_name = os.environ.get("DATABASE_NAME")
+
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"{database_pre}{database_user}@{database_addr}:{database_port}/{database_name}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+init_db(app, db)
+ma = Marshmallow(app)
+
+
+def create_all():
+    with app.app_context():
+        print("Creating Tables")
+        db.create_all()
+        print("Tables created")
+
+# app.register_blueprint(routes.)
+# app.register_blueprint(routes.)
+# app.register_blueprint(routes.)
+# app.register_blueprint(routes.)
+# app.register_blueprint(routes.)
+
+
+if __name__ == "__main__":
+    create_all()
+    app.run(port=8086, host="0.0.0.0", debug=True)

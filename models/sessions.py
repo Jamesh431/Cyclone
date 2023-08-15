7223b93b03963857174621b962c6a9b4cb5df813
James Hales <jdhales2686@gmail.com>
2023-08-14 20:39:20 -0600

    uses correct data types

--- models/sessions.py
+++ models/sessions.py
@@ -6,23 +6,23 @@ from sqlalchemy_utils import JSONType
 
 from db import db
 from models.repositories import RepoSchema
-from user_sessions_xref import user_sessions_xref
+from models.user_sessions_xref import user_sessions_xref
 
 
-class Sessions(db.model):
+class Sessions(db.Model):
     __tablename__ = "Sessions"
 
     session_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     current_repo = db.Column(UUID(as_uuid=True), db.ForeignKey("Repositories.repo_id"))
-    num_of_commits = db.Column(db.integer(), default=1, nullable=False)
+    num_of_commits = db.Column(db.Integer(), default=1, nullable=False)
     commit_by_repo_ammount = db.Column(db.Boolean(), default=True, nullable=False)  # committing by a num of repos or a num of commits
-    time_to_commit = db.Column(db.DateTime.time())  # default=datetime.combine(datetime.min, time(8, 17)
-    time_frame = db.Column(db.JSONType(), default={"8:17": "8:17"}, nullable=False)
+    time_to_commit = db.Column(db.Time())  # default=datetime.combine(datetime.min, time(8, 17)
+    time_frame = db.Column(JSONType(), default={"8:17": "8:17"}, nullable=False)
     latest_commit = db.Column(db.DateTime())
-    current_position = db.Column(db.integer(), default=0, nullable=False)
+    current_position = db.Column(db.Integer(), default=0, nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
 
-    users = db.relationship('Users', secondary=user_sessions_xref, backpopulates='session')
+    users = db.relationship('Users', secondary=user_sessions_xref, back_populates='session')
 
     def __init__(self, current_repo, num_of_commits, commit_by_repo_ammount, time_to_commit, time_frame, latest_commit, current_position, active):
         self.current_repo = current_repo

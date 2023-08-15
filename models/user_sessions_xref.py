3c461a90484c0fc1e496401bfcd0613f7674fc4b
James Hales <jdhales2686@gmail.com>
2023-08-14 20:39:49 -0600

    added metadata

--- models/user_sessions_xref.py
+++ models/user_sessions_xref.py
@@ -3,7 +3,8 @@ from sqlalchemy.dialects.postgresql import UUID
 
 from db import db
 
-user_sessions_xref = db.table("UserSessionsXref",
+user_sessions_xref = db.Table("UserSessionsXref",
+                              db.Model.metadata,
                               session_id=db.Column(UUID(as_uuid=True), db.ForeignKey("Sessions.session_id"), primary_key=True),
                               receiver_id=db.Column(UUID(as_uuid=True), db.ForeignKey("Users.user_id"), primary_key=True),
                               sender_id=db.Column(UUID(as_uuid=True), db.ForeignKey("Users.user_id"), primary_key=True))

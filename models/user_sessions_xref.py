b7c515288215dcda29b8995a65f584b168d947f4
James Hales <jdhales2686@gmail.com>
2023-08-15 19:23:09 -0600

    added imports and changed referencing

--- models/user_sessions_xref.py
+++ models/user_sessions_xref.py
@@ -1,10 +1,12 @@
 import marshmallow as ma
 from sqlalchemy.dialects.postgresql import UUID
+from .sessions import Sessions
+from .users import Users
 
 from db import db
 
 user_sessions_xref = db.Table("UserSessionsXref",
                               db.Model.metadata,
-                              session_id=db.Column(UUID(as_uuid=True), db.ForeignKey("Sessions.session_id"), primary_key=True),
-                              receiver_id=db.Column(UUID(as_uuid=True), db.ForeignKey("Users.user_id"), primary_key=True),
-                              sender_id=db.Column(UUID(as_uuid=True), db.ForeignKey("Users.user_id"), primary_key=True))
+                              session_id=db.Column(UUID(as_uuid=True), db.ForeignKey(Sessions.session_id), primary_key=True),
+                              receiver_id=db.Column(UUID(as_uuid=True), db.ForeignKey(Users.user_id), primary_key=True),
+                              sender_id=db.Column(UUID(as_uuid=True), db.ForeignKey(Users.user_id), primary_key=True))

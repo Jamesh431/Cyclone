826732c705909030b9a1167699e68a17726478a6
James Hales <jdhales2686@gmail.com>
2023-08-18 13:50:55 -0600

    commented out code

--- controllers/sessions_controller.py
+++ controllers/sessions_controller.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import aliased
 
 from db import db
 from models.sessions import Sessions, session_schema, sessions_schema
-from models.users import Users
+from models.users import Users, user_schema
 from models.user_sessions_xref import user_sessions_xref
 from util.reflection import populate_obj
 
@@ -38,24 +38,29 @@ def add_session(req: Request):
     populate_obj(new_session, post_data)
 
     db.session.add(new_session)
-    if receiver_id:
-        # user_object = db.session.get(Users, receiver_id)
-        print(added_session)
-        added_session.users.append(user)
+    # if receiver_id:
+    # user_object = db.session.get(Users, receiver_id)
+    # print(user)
+    # dumped_user = user_schema.dump(user)
+    # print(dumped_user["user_id"])
+    # new_session.users.append(dumped_user["user_id"])
     db.session.commit()
 
-    return jsonify(session_schema.dump(new_session))
+    # return jsonify(session_schema.dump(new_session))
 
-    # session_data = session_schema.dump(new_session)
+    session_data = session_schema.dump(new_session)
+    print(session_data)
 
-    # if receiver_id:
-    #     fetched_session = db.session.query(Sessions).filter(Sessions.session_id == session_data.session_id)
+    if receiver_id:
+        fetched_session = db.session.query(Sessions).filter(Sessions.session_id == session_data['session_id']).first()
 
-    #     fetched_session.user.append(user_query)
+        print(fetched_session)
 
-    #     populate_obj(fetched_session, post_data)
+        fetched_session.users.append(user)
 
-    # return jsonify({"message": "session created", "session info": session_data}), 201
+        session_data = fetched_session
+
+    return jsonify({"message": "session created", "session info": session_data}), 201
 
 
 def get_all_sessions(req: Request):

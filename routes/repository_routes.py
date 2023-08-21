65fe75377ab6dcac924d49261900808600387574
James Hales <jdhales2686@gmail.com>
2023-08-21 16:39:22 -0600

    added CRUD+ repository routes

--- routes/repository_routes.py
+++ routes/repository_routes.py
@@ -2,9 +2,49 @@ from flask import request, Response, Blueprint
 
 from controllers import repository_controller as controller
 
-repositories = Blueprint("repositories", __name__)
+repos = Blueprint("repositories", __name__)
 
 
-@repositories.route('/repository', methods=["POST"])
+@repos.route('/repo', methods=["POST"])
 def add_repo() -> Response:
     return controller.add_repository(request)
+
+
+@repos.route('/repo/<repo_id>', methods=['GET'])
+def get_repo_by_id(repo_id) -> Response:
+    return controller.get_repository(request, repo_id)
+
+
+@repos.route('/repo/search', methods=['GET'])
+def get_repo_by_search() -> Response:
+    return controller.get_repository_by_search(request)
+
+
+@repos.route('/repo/<sender_id>', methods=['GET'])
+def get_repos_by_sender_id(sender_id) -> Response:
+    return controller.get_repositories_by_sender_id(request, sender_id)
+
+
+@repos.route('/repos', methods=['GET'])
+def get_all_repos() -> Response:
+    return controller.get_all_repositories(request)
+
+
+@repos.route('/repos/active', methods=['GET'])
+def get_active_repos() -> Response:
+    return controller.get_active_repositories(request)
+
+
+@repos.route('/repo/<repo_id>', methods=["PATCH"])
+def update_repo(repo_id) -> Response:
+    return controller.update_repository(request, repo_id)
+
+
+@repos.route('/repo/<repo_id>', methods=["DELETE"])
+def delete_repo(repo_id) -> Response:
+    return controller.delete_repository(request, repo_id)
+
+
+@repos.route('/repo/<repo_id>', methods=["PATCH"])
+def repo_activity(repo_id) -> Response:
+    return controller.repository_activity(request, repo_id)

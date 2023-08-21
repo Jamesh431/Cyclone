7bd7ccc82a55595c4e1efdb114d369aceee7a7e9
James Hales <jdhales2686@gmail.com>
2023-08-21 16:45:07 -0600

    added CRUD route for commits

---
+++ routes/commit_routes.py
@@ -0,0 +1,35 @@
+from flask import request, Response, Blueprint
+
+from controllers import commits_controller as controller
+
+commits = Blueprint("commits", __name__)
+
+
+@commits.route('/commit', methods=["POST"])
+def add_commit() -> Response:
+    return controller.add_commit(request)
+
+
+@commits.route('/commits', methods=['GET'])
+def get_commits() -> Response:
+    return controller.get_all_commits(request)
+
+
+@commits.route('/commit/<commit_id>', methods=['GET'])
+def get_commit_by_id(commit_id) -> Response:
+    return controller.get_commit(request, commit_id)
+
+
+@commits.route('/commit/<commit_id>', methods=['GET'])
+def get_commits_by_repo_id(repo_id) -> Response:
+    return controller.get_commits_by_repo_id(request, repo_id)
+
+
+@commits.route('/commit/<commit_id>', methods=["PATCH"])
+def update_commit(commit_id) -> Response:
+    return controller.update_commit(request, commit_id)
+
+
+@commits.route('/commit/<commit_id>', methods=["DELETE"])
+def delete_commit(commit_id) -> Response:
+    return controller.delete_commit(request, commit_id)

66e62af3dd3ca945d9ab2c920104852ecdbafd83
James Hales <jdhales2686@gmail.com>
2023-08-21 15:19:01 -0600

    added ssh_key

--- models/repositories.py
+++ models/repositories.py
@@ -14,24 +14,26 @@ class Repositories(db.Model):
     repo_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     sender_id = db.Column(UUID(as_uuid=True), db.ForeignKey("Users.user_id"), nullable=False)
     name = db.Column(db.String(), nullable=False)
+    ssh_key = db.Column(db.String(), nullable=False)
     branches = db.Column(ListType())
     active = db.Column(db.Boolean(), default=True, nullable=False)
 
     # assigned_sessions = db.relationship('Sessions', secondary=session_repo_xref, back_populates='assigned_repos')
 
-    def __init__(self, sender_id, name, branches, active):
+    def __init__(self, sender_id, name, ssh_key, branches, active):
         self.sender_id = sender_id
         self.name = name
+        self.ssh_key = ssh_key
         self.branches = branches
         self.active = active
 
     def new_repository():
-        return Repositories("", "", "", True)
+        return Repositories("", "", "", "", True)
 
 
 class RepoSchema(ma.Schema):
     class Meta:
-        fields = ['repo_id', 'sender_id', 'name', 'branches', 'active']
+        fields = ['repo_id', 'sender_id', 'name', 'ssh_key', 'branches', 'active']
 
         sender_id = ma.fields.Nested(UserSchema)
         # assigned_sessions = ma.fields.Nested('SessionSchema', many=True, only=['session_id', 'name', 'active'])
